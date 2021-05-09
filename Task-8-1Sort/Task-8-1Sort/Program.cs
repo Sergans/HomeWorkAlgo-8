466a4bed979d0cc3da0a902a58057219e56f0b7a
Сергей <gan7er@yandex.ru>
2021-05-09 15:06:17 +0300

    Без теста

--- Task-8-1Sort/Task-8-1Sort/Program.cs
+++ Task-8-1Sort/Task-8-1Sort/Program.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace Task_8_1Sort
 {
     class Program
     {
-        static void Main(string[] args)
+        static int[] BubleAr(int []a)
         {
-            int[] a = { 1, 3, 6, 4, 2, 9, 5,7,12,1 };
             for (int j = 0; j < a.Length; j++)
             {
                 for (int i = 0; i < a.Length - 1; i++)
@@ -20,8 +19,75 @@ namespace Task_8_1Sort
                     }
                 }
             }
-            foreach (int b in a)
+            return a;
+        }
+        static List <int> BubleLs(List<int>a)
+        {
+            for (int j = 0; j < a.Count; j++)
+            {
+                for (int i = 0; i < a.Count - 1; i++)
+                {
+                    if (a[i] > a[i + 1])
+                    {
+                        int tmp = a[i + 1];
+                        a[i + 1] = a[i];
+                        a[i] = tmp;
+
+                    }
+                }
+            }
+            return a;
+        }
+        static int[] BucketSort(int[]a)
+        {
+            List<int>[] bucket = new List<int>[a.Length];
+            for (int i = 0; i < bucket.Length; i++)
+            {
+                bucket[i] = new List<int>();
+
+            }
+            int minValue = a[0];
+            int maxValue = a[0];
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (a[i] < minValue)
+                    minValue = a[i];
+                else if (a[i] > maxValue)
+                    maxValue = a[i];
+            }
+            double numRange = maxValue - minValue;
+            for (int i = 0; i < a.Length; ++i)
+            {
+
+                int bucketIndex = (int)Math.Floor((a[i] - minValue) / numRange * (bucket.Length - 1));
+
+
+                bucket[bucketIndex].Add(a[i]);
+            }
+            for (int i = 0; i < bucket.Length; i++)
+                BubleLs(bucket[i]);
+
+
+            int idx = 0;
+
+            for (int i = 0; i < bucket.Length; i++)
+            {
+                for (int j = 0; j < bucket[i].Count; j++)
+                    a[idx++] = bucket[i][j];
+            }
+            return a;
+        }
+
+        static void Main(string[] args)
+        {
+
+            int[] a = { 1, 3, 6, 4, 2, 9, 5,7,12,1 };
+            
+
+            foreach (int b in BucketSort(a))
+            {
                 Console.WriteLine(b);
+            }
         }
     }
 }
